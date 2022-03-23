4f6eb7576ac152a6ef208009fec88010178ab1e3
taicout6 <taicout6@gmail.com>
2022-03-23 16:58:56 -0300

    Adicionando validações de email e password

---
+++ app/backend/src/database/validations/loginValidations.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from 'express';
+import * as joi from 'joi';
+// import * as bcryptjs from 'bcryptjs';
+import ILogin from '../../interfaces/Login';
+// import UserModel from '../models/UserModel';
+
+const emailSchema = joi.object({
+  email: joi.string().email().required(),
+});
+
+export const emailValidation = (req: Request, res: Response, next: NextFunction) => {
+  const { email }: ILogin = req.body;
+  if (!email) {
+    res.status(401).json({ message: 'All fields must be filled' });
+  }
+  const { error } = emailSchema.validate({ email });
+  if (error) {
+    res.status(401).json({ message: 'Incorrect email or password' });
+  }
+  next();
+};
+
+const passwordSchema = joi.object({
+  password: joi.string().min(7).required(),
+});
+
+export const passwordValidation = (req: Request, res: Response, next: NextFunction) => {
+  const { password }: ILogin = req.body;
+  if (!password) {
+    res.status(401).json({ message: 'All fields must be filled' });
+  }
+  const { error } = passwordSchema.validate({ password });
+  if (error) {
+    res.status(401).json({ message: 'Incorrect email or password' });
+  }
+  next();
+};
--- app/backend/src/routes/loginRouter.ts
+++ app/backend/src/routes/loginRouter.ts
@@ -1,12 +1,13 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
+import { emailValidation, passwordValidation } from '../database/validations/loginValidations';
 import LoginController from '../database/controllers/LoginController';
 
 const loginRoute = express.Router();
 
 loginRoute
   .route('/login')
-  .post(LoginController)
+  .post(emailValidation, passwordValidation, LoginController)
   .get((_req: Request, res: Response) => { res.status(200).json('Hello World'); });
 
 export default loginRoute;
