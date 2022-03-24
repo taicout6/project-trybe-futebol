449990c6942fb1b5c0689ea5c543d17496978e08
taicout6 <taicout6@gmail.com>
2022-03-24 12:39:26 -0300

    Implementando getAllClubs

--- app/backend/src/database/controllers/ClubsController.ts
+++ app/backend/src/database/controllers/ClubsController.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from 'express';
+import ClubModel from '../models/ClubModel';
 
-export const getAllClubs = (_req: Request, res: Response) => {
-  res.status(200).json('All clubs');
+export const getAllClubs = async (_req: Request, res: Response) => {
+  const clubs = await ClubModel.findAll();
+  res.status(200).json(clubs);
 };
 
-export const getClubById = (req: Request, res: Response) => {
+export const getClubById = async (req: Request, res: Response) => {
   const { id } = req.params;
   console.log('Not implemented');
   res.status(200).json(id);
