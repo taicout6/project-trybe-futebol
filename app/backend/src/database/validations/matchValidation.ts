6d58139df021bda643f2e5ebcce13a3d81cc8743
taicout6 <taicout6@gmail.com>
2022-03-25 17:40:27 -0300

    Requisito 26, não deve ser possivel criar uma partida com um time que não existe no banco de dados -> func matchValidate

--- app/backend/src/database/validations/matchValidation.ts
+++ app/backend/src/database/validations/matchValidation.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import ClubModel from '../models/ClubModel';
 
 const matchValidate = async (req: Request, res: Response, next: NextFunction) => {
   const { homeTeam, awayTeam } = req.body;
@@ -6,6 +7,11 @@ const matchValidate = async (req: Request, res: Response, next: NextFunction) =>
     return res.status(401).json({
       message: 'It is not possible to create a match with two equal teams',
     });
+  }
+  const homeClubExist = await ClubModel.findByPk(homeTeam);
+  const awayClubExist = await ClubModel.findByPk(awayTeam);
+  if (!homeClubExist || !awayClubExist) {
+    return res.status(404).json({ message: 'There is no team with such id!' });
   }
   next();
 };
